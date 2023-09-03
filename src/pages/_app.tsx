c095878742a272c91e2c58eb4c17d939d0fd5c54
hamzaeldemery <hamzaeldemery@gmail.com>
2023-09-03 23:52:57 +0300

    add custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/style/global.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
+
+const surt = localFont({
+  src: "../assets/fonts/GEDinkum-Bold-1.ttf",
+  variable: "--font-surt-bold",
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <main className={`${surt.variable} font-sans`} dir="rtl">
       <Navbar />
       <Component {...pageProps} />
-    </div>
+    </main>
   );
 }
