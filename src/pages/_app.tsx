a6f82a3f5aa20ce365e203da70362d52a1ae034a
hamzaeldemery <hamzaeldemery@gmail.com>
2023-09-03 23:09:36 +0300

    create navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import "@/style/global.css";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
