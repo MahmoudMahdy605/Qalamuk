e85dd64619333e99035e3c59c0e463aeb1628763
hamzaeldemery <hkhehmad@gmail.com>
2023-09-04 01:26:21 +0300

    add some testing context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { Stack } from "@mui/material";
+
 const Page = () => {
-  return <div>App home page</div>;
+  return (
+    <Stack height={"100%"} sx={{ overflow: "scroll", mt: "70px" }}>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+      <Stack m={3}>App home page</Stack>
+    </Stack>
+  );
 };
 
 export default Page;
